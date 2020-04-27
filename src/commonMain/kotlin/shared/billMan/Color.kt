0ebff91d4a3664ba06b31719299572f260eb6074
Adam Weesner <alweesner@gmail.com>
2020-04-26 22:32:14 -0500

    forgot to make color a GenericItem

--- src/commonMain/kotlin/shared/billMan/Color.kt
+++ src/commonMain/kotlin/shared/billMan/Color.kt
@@ -1,5 +1,9 @@
 package shared.billMan
 
+import shared.base.GenericItem
+import shared.base.History
+import shared.currentTimeMillis
+
 /**
  * The color to be shown to the user for specific items.
  *
@@ -9,8 +13,12 @@ package shared.billMan
  * @param alpha The alpha channel of the color.
  */
 data class Color(
+    override var id: Int? = null,
     var red: Int,
     var green: Int,
     var blue: Int,
-    var alpha: Int
-)
+    var alpha: Int,
+    override var history: MutableList<History>? = null,
+    override val dateCreated: Long = currentTimeMillis(),
+    override val dateUpdated: Long = currentTimeMillis()
+) : GenericItem
